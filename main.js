1d3e5cce528cb3f4a21b7553e0387c1de7a8f07a
CFeeney333 <c.feeney333@gmail.com>
2022-09-04 15:24:56 +0100

    Add base functionality to library with test data

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// global variables
+const myLibrary = [];
+
+// global functions
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function removeBookFromLibrary() {}
+
+function displayBooks() {
+  for (let book of myLibrary) {
+    console.log(book.info());
+  }
+}
+
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
@@ -11,5 +27,24 @@ Book.prototype.info = function () {
   }`;
 };
 
+Book.prototype.changeReadStatus = function (isRead) {
+  this.isRead = isRead;
+};
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+const narnia = new Book(
+  "The Lion, The Witch and the Wardrobe",
+  "C.S. Lewis",
+  153,
+  true
+);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(narnia);
+displayBooks();
+console.log("Changing read status for the hobbit");
+theHobbit.changeReadStatus(true);
+displayBooks();
+console.log("Changing it back");
+theHobbit.changeReadStatus(false);
+displayBooks();
